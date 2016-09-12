599e91c4bfa8f9b61fdddf7036e486f2acc59110
Michel CANTACUZENE <mcanta5@ds2.CS.McGill.CA>
2016-09-12 11:56:09 -0400

    Template from ECSE 202 2016-09-12 lecture 4

---
+++ template.c
@@ -0,0 +1,19 @@
+/*
+================================================
+Name		: main.c
+Author		: Michel Cantcuzene
+Version		: 0.1
+Copyright	: Your copyright notice
+Description	: Hello World in C, Ansi style
+================================================
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+	// you can write main(int argc, *argv[])
+	puts("hello World");
+	return EXIT_SUCCESS;
+}
