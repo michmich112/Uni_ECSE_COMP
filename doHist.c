b9be892b579b438250a48dc39dd03c0414c0e32c
Michel CANTACUZENE <mcanta5@ds2.CS.McGill.CA>
2016-09-13 21:51:05 -0400

    Arranged the files, completed the doHist.c

--- doHist.c
+++ doHist.c
@@ -1,13 +1,31 @@
 #include <stdio.h>
 
-void toZero(char hist[]){
-	
+// We know that hist is a 256 wide array
+
+int size(char text[]){
+	int start = 0;
+	char character = text[0];
+	while(character != '\0'){
+		start += 1;
+		character = text[start];
+	}
+	return start;
 }
 
-int doHist(char buffer[], char hist[]){
-	
+void doHist(char buffer[], char hist[]){
+	for(int i=0;i<256;i++){
+		hist[i]=0;
+	}
+	for(int i=0;i<size(buffer);i++){
+		hist[buffer[i]] += 1;
+	}
 }
 
 int main(int argc, char *argv[]){
-
+	char text[] = "Jesuisbobdacords";
+	char hist[256];
+	doHist(text,hist);
+	for (int i=0;i<256;i++){
+		printf("%d \t",hist[i]);
+	}
 }
--- tests/fill_array.c
+++ tests/fill_array.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]){
-	char test[256];
+	char test[256]; //I wanted to see what values would be stored inside a 256 long array
 	for (int i=0;i<256;i++){
 		printf("%d \t",test[i]);
 	}
