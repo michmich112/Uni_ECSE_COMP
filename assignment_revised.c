5745778bd2b3704e06ea1006c320bd2368e900b2
Michel Cantacuzene <Michel@Michels-MacBook-Pro.local>
2016-09-16 12:47:06 -0400

    revised the assignment and made a more compact and more rapid version

---
+++ assignment_revised.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+void getHist(char text[], char hist[]){
+	int i=0;
+	while(text[i] != '\0'){
+		hist[text[i]] += 1;
+	}
+}
+
+
+int getMax(char hist[]){
+	int max = 0;
+	for(int i=0;i<256;i++){
+		if (hist[i]>max){max=hist[i];}
+	}
+	return max;
+}
+
+void displayHist(char hist[]){
+	int max = getMax(hist);
+	int MAXSCALE = 10;
+	int barlength;
+	for (int i=0;i<256;i++){
+		if (hist[i] != 0){
+			barlength = (int)(((double)hist[i])/((double)max)*((double)MAXSCALE));
+			printf("%c \t [%d] \t",i,hist[i]);for(int i=0;i<barlength;i++){printf("*");} //display everything pertaining to the bar 
+			printf("\n"); //print the newline character to tell the console to got to a new line.
+		}
+	}
+}
+
+int main(int argc, char *argv[]){
+	char hist[256];
+	for(int i=0;i<256;i++){hist[i]=0;} //put all of the values in hist to 0
+	for(int i=1;i<argc;i++){
+		getHist(argv[i],hist);
+	}
+	displayHist(hist);
+	return 0;
+}
